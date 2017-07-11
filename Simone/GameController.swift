1a9b57709577106d87b4421496cbe5e62577dd3d
eleves <eleves@mac404inf-09.local>
2017-07-11 17:18:34 -0400

    correction timer

--- Simone/GameController.swift
+++ Simone/GameController.swift
@@ -56,6 +56,12 @@ class GameController: UIViewController {
     //-----------------------------------
     
     @IBAction func buttonManager(_ sender: UIButton) {
+        if simoneBrain.aTimer == nil || simoneBrain.userTurnToPlay == nil {
+            return
+        }
+        simoneBrain.aTimer.invalidate()
+        timerLabel.text = ""
+        
         
         if !simoneBrain.userTurnToPlay{
             
